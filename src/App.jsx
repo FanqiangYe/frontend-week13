5cddf3c732a3e99db95a773010dd4b713c585013
fanqiangye <fanqiang_ye@hotmail.com>
2024-02-29 18:15:34 +0100

    klein begin gemaakt, verder werken vanaf PC

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import axios from 'axios';
 import './App.css';
 
 function App() {
+    async function countries() {
+        try {
+            const result = await axios.get('https://restcountries.com/v3.1/all')
+            console.log(result.data)
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+
+
 
     return (
         <>
-            Maak je applicatie hier!
+            <button onClick={handleClick}>Klik hier voor alle landen</button>
         </>
     )
 }
