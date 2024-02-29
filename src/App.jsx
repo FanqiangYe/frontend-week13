b9f5d30631c485f2bc3e049a288fd0213060fe83
FanqiangYe <fanqiang_ye@hotmail.com>
2024-02-29 21:40:45 +0100

    Verder vanaf opdracht 1 stap 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import axios from 'axios';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
-    async function countries() {
-        try {
-            const result = await axios.get('https://restcountries.com/v3.1/all')
-            console.log(result.data)
-        } catch (e) {
-            console.error(e);
-        }
+    const [countries, setCountries] = useState([]);
+async function fetchData(){
+    try {
+        const response = await axios.get('https://restcountries.com/v3.1/all')
+        console.log(response.data)
+        setCountries(response.data);
+    } catch (e) {
+        console.error(e)
     }
-
-
+}
 
 
     return (
         <>
-            <button onClick={handleClick}>Klik hier voor alle landen</button>
+            <button onClick={fetchData} type="button">
+                Haal landen op
+            </button>
+            <ul>
+                {countries.map((country) => {
+                    return <li key={country}>{country[0]}</li>
+                })}
+            </ul>
+
         </>
     )
 }
